b031327284f3d211a4d14bac73e826ef0b5303b4
Damyan Maxwell <saintnyte@gmail.com>
2025-05-13 10:01:44 +0200

    02 data/01 af

---
+++ sql/02 data/huisdierdata.sql
@@ -0,0 +1,8 @@
+INSERT INTO `huisdieren` 
+(`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+
+VALUES 
+(NULL, 'Zeus', 'Damyan', 'Siberian Husky cross bred with pitbull', '3', '2021-07-17'),
+(NULL, 'Athena', 'Damyan', 'Mixed Pitbull', '2', '2021-08-18'),
+(NULL, 'Odin', 'Mixed', '1', '2022-05-19'),
+(NULL, 'Hades', 'Chris', 'Mixed', '1', '2022-05-19');
