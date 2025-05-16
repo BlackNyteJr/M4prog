290d74bf9263c79be8d79207a0a3da36e70f841e
Damyan Maxwell <saintnyte@gmail.com>
2025-05-16 09:58:37 +0200

    03 complex select/03 af

---
+++ sql/03 complex select/03 order.sql
@@ -0,0 +1,5 @@
+SELECT * FROM `huisdieren` order by naam 
+SELECT * FROM `huisdieren` order by naam ASC
+SELECT * FROM `huisdieren` order by naam DESC
+SELECT * FROM `huisdieren` order by typedier, naam
+SELECT * FROM `huisdieren` order by typedier DESC, naam ASC
