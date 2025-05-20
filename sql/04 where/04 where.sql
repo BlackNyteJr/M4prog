f00779cfa35ed3929945d894f437042e0046a99e
Damyan Maxwell <saintnyte@gmail.com>
2025-05-20 09:29:30 +0200

    04 where/01 af

---
+++ sql/04 where/04 where.sql
@@ -0,0 +1,5 @@
+SELECT * FROM `huisdieren` WHERE leeftijd <= 4;
+SELECT * FROM `huisdieren` WHERE leeftijd > 4;
+SELECT * FROM `huisdieren` WHERE leeftijd = 4;
+SELECT naam, leeftijd FROM `huisdieren` WHERE naam = 'Zeus';
+SELECT id, typedier FROM `huisdieren` WHERE id <= 5;
