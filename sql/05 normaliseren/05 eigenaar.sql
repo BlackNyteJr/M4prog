4ccfbfbc3054c039153b54a138b2337e5750fb28
Damyan Maxwell <saintnyte@gmail.com>
2025-05-23 09:42:40 +0200

    05 normaliseren/01 af

---
+++ sql/05 normaliseren/05 eigenaar.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Generation Time: May 23, 2025 at 07:31 AM
+-- Server version: 11.7.2-MariaDB-ubu2404
+-- PHP Version: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m4prog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `huisdiereigenaar`
+--
+
+CREATE TABLE `huisdiereigenaar` (
+  `id` int(11) NOT NULL,
+  `voornaam` varchar(120) NOT NULL,
+  `achternaam` varchar(120) NOT NULL,
+  `leeftijd` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+INSERT INTO `huisdiereigenaar` 
+(`id`, `voornaam`, `achternaam`, `leeftijd`)
+
+VALUES 
+(1, 'Damyan','-', '7'),
+(2, 'Chris','-', '7'),
+(3, 'Joep','-', '7'),
+(4, 'Marije','-', '7');
