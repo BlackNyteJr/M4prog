020a19a72f5112c660e56bbe93976a507dfd9c43
Damyan Maxwell <saintnyte@gmail.com>
2025-05-27 10:06:30 +0200

    06 join/02 af

---
+++ sql/06 join/06 join.sql
@@ -0,0 +1,2 @@
+ SELECT * FROM hobby as h join persoon as p on h.hobbyid = p.hobbyid;
+SELECT h.hobbyid AS hobby, hobby, p.id AS persoonid, p.naam FROM hobby AS h JOIN persoon AS p ON h.hobbyid = p.hobbyid;
