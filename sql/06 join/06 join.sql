767da33e1d3963ff7ff4caa559f0b71a07f33d08
Damyan Maxwell <saintnyte@gmail.com>
2025-05-27 10:17:55 +0200

    aanpassing in 05

--- sql/06 join/06 join.sql
+++ sql/06 join/06 join.sql
@@ -1,2 +1,3 @@
- SELECT * FROM hobby as h join persoon as p on h.hobbyid = p.hobbyid;
+SELECT * FROM hobby as h join persoon as p on h.hobbyid = p.hobbyid;
 SELECT h.hobbyid AS hobby, hobby, p.id AS persoonid, p.naam FROM hobby AS h JOIN persoon AS p ON h.hobbyid = p.hobbyid;
+
