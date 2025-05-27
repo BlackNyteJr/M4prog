cd6fc7d30abf34801767e651a777629aa6327e9e
Damyan Maxwell <saintnyte@gmail.com>
2025-05-27 10:33:43 +0200

    06 join/03 af

--- sql/06 join/06 join.sql
+++ sql/06 join/06 join.sql
@@ -1,3 +1,4 @@
 SELECT * FROM hobby as h join persoon as p on h.hobbyid = p.hobbyid;
 SELECT h.hobbyid AS hobby, hobby, p.id AS persoonid, p.naam FROM hobby AS h JOIN persoon AS p ON h.hobbyid = p.hobbyid;
 
+SELECT t.klantid, gesprekStart, gesprekEind, gesprekNaarTelefoonnummer, k.klantid, naam, leeftijd, telefoonnummer FROM `telefoonGesprek` as t join klant as k on t.klantid = k.klantid;
