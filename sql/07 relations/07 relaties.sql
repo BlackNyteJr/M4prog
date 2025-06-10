c12d15abffac6febe2c33b7c703ed3805f47b7b8
Damyan Maxwell <saintnyte@gmail.com>
2025-06-10 09:35:26 +0200

    07 relations/02 af

--- sql/07 relations/07 relaties.sql
+++ sql/07 relations/07 relaties.sql
@@ -1,6 +1,10 @@
-
 insert into ingredient (id,naam) values 
 (1,'pindas');
-
 insert into voedselProduct (hoofdIngredientId,naam) values 
 (1,'pindakaas');
+
+INSERT INTO ingredient (id, naam) VALUES (2, 'honing');
+INSERT INTO voedselProduct (hoofdIngredientId, naam) VALUES (2, 'honingkoek');
+
+INSERT INTO ingredient (id, naam) VALUES (3, 'bosbessen');
+INSERT INTO voedselProduct (hoofdIngredientId, naam) VALUES (3, 'bosbessenkwark');
